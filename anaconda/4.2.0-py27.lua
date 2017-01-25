c8618e5e158ac87cdff56795998b0f815eeb3344
oesteban <code@oscaresteban.es>
2017-01-25 12:05:01 -0800

    update anaconda paths and versions

--- anaconda/4.2.0-py27.lua
+++ anaconda/4.2.0-py27.lua
@@ -6,8 +6,8 @@ running mriqc
 Version 1.5.1
 ]])
 
-whatis("Name: anaconda")
-whatis("Version: 1.5.1")
+whatis("Name: Anaconda Python 2")
+whatis("Version: 4.2.0")
 whatis("Category: tools, python")
 whatis("Keywords: python anaconda")
 whatis("Description: anaconda")
@@ -18,7 +18,7 @@ always_unload("python")
 
 -- Anaconda path
 local software_path = '/work/01329/poldrack/software'
-local anaconda_path = pathJoin(software_path, 'anaconda')
+local anaconda_path = pathJoin(software_path, 'anaconda2-4.2.0')
 
 -- Start miniconda
 prepend_path('PATH', pathJoin(anaconda_path, 'bin'));
