9de80cbdc751ca0ba2c161024a5232247e3d5a2c
oesteban <code@oscaresteban.es>
2017-01-25 13:00:40 -0800

    close if statements in crnenv

--- crnenv/1.0.0.lua
+++ crnenv/1.0.0.lua
@@ -15,14 +15,17 @@ if string.match(hostname, "stanford.edu") then
 	-- setenv('CRNENV_WORK', '/work/01329/poldrack')
 	-- setenv('CRNENV_SINGULARITY_IMAGES', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
 	-- append_path('PATH', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+end
 
 if string.match(hostname, "stampede") then
 	setenv('CRNENV_WORK', '/work/03843/crn_plab')
 	setenv('CRNENV_SINGULARITY_IMAGES', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
 	append_path('PATH', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+end
 
 if string.match(hostname, "ls5") then
 	setenv('CRNENV_WORK', '/work/03843/crn_plab')
+end
 
 setenv('CRNENV_EXECUTION_SYSTEM', hostname);
 setenv('CRNENV_SYSTEM_NCPUS', ncpus);
