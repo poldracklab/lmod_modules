bcf4314f4ed0e808be44481c61be9b7f89a7bc5d
oesteban <code@oscaresteban.es>
2017-01-25 12:58:53 -0800

    use capture in crnenv

--- crnenv/1.0.0.lua
+++ crnenv/1.0.0.lua
@@ -6,11 +6,23 @@ load("fsl")
 load("freesurfer")
 load("anaconda")
 
-
-setenv('CRNENV_WORK', '/work/03843/crn_plab')
-
 -- Initialize variable names
-setenv('CRNENV_EXECUTION_SYSTEM', 'stampede.tacc.utexas.edu');
-setenv('CRNENV_SYSTEM_NCPUS', 16);
-setenv('CRNENV_SINGULARITY_IMAGES', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
-append_path('PATH', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+local ncpus = capture("grep -c '^processor' /proc/cpuinfo")
+local hostname = capture("/bin/hostname -d")
+
+if string.match(hostname, "stanford.edu") then
+	hostname = "sherlock.stanford.edu"
+	-- setenv('CRNENV_WORK', '/work/01329/poldrack')
+	-- setenv('CRNENV_SINGULARITY_IMAGES', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+	-- append_path('PATH', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+
+if string.match(hostname, "stampede") then
+	setenv('CRNENV_WORK', '/work/03843/crn_plab')
+	setenv('CRNENV_SINGULARITY_IMAGES', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+	append_path('PATH', pathJoin(os.getenv('CRNENV_WORK'), 'singularity'));
+
+if string.match(hostname, "ls5") then
+	setenv('CRNENV_WORK', '/work/03843/crn_plab')
+
+setenv('CRNENV_EXECUTION_SYSTEM', hostname);
+setenv('CRNENV_SYSTEM_NCPUS', ncpus);
