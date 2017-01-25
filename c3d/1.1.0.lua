f6a651ed77b8f47f06e17f20d7ef03586ddf0ea8
oesteban <code@oscaresteban.es>
2017-01-24 17:58:28 -0800

    add jq and c3d

---
+++ c3d/1.1.0.lua
@@ -0,0 +1,17 @@
+help (
+[[
+This module loads C3D - converts FSL-like and ANTS-like affine transforms.
+
+Version 1.1.0
+]])
+
+whatis("Name: C3D")
+whatis("Version: 1.1.0")
+whatis("Category: computational biology, Imaging")
+whatis("Keywords:  Biology, fMRI, Reconstruction")
+whatis("Description: C3D")
+whatis("URL: ")
+
+local software_path = '/work/03843/crn_plab/software'
+local c3d_path = pathJoin(software_path, 'c3d-1.1.0')
+prepend_path('PATH', pathJoin(c3d_path, 'bin'));
---
+++ jq/1.5.lua
@@ -0,0 +1,18 @@
+help (
+[[
+This module loads JQ - handle JSON in bash.
+
+Version 1.5
+]])
+
+whatis("Name: jq")
+whatis("Version: 1.5")
+whatis("Category: tools, javascript")
+whatis("Keywords: json")
+whatis("Description: jq")
+whatis("URL: ")
+
+local software_path = '/work/03843/crn_plab/software'
+local jq_path = pathJoin(software_path, 'jq-1.5')
+prepend_path('PATH', pathJoin(jq_path, 'bin'));
+prepend_path('LD_LIBRARY_PATH', pathJoin(jq_path, 'lib'));
