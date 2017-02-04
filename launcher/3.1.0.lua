73b9553e0b9fb0df8a499b5a5f4aff556dd38c06
Oscar Esteban <code@oscaresteban.es>
2017-02-03 18:27:10 -0800

    launcher now checks for python > 2.6

--- launcher/3.1.0.lua
+++ launcher/3.1.0.lua
@@ -1,27 +1,35 @@
 help (
 [[
-The Launcher is a utility for construction simple HTC workflows bundled 
+The Launcher is a utility for construction simple HTC workflows bundled
 as a single batch job.
 
 The LAUNCHER module defines the LAUNCHER_DIR environment variable.
 
-For more information on using the Launcher, please consult 
+For more information on using the Launcher, please consult
 LAUNCHER_DIR/README or go to the Launcher website:
 
 https://www.tacc.utexas.edu/research-development/tacc-software/the-launcher.
 ]])
 
 whatis("Name: Launcher")
-whatis("Version: 3.0.1")
+whatis("Version: 3.1.0")
 
-always_unload("crnenv")
-load("python")
+-- Check Python version: launcher requires >=2.7
+local pyversion = capture("python --version")
+if string.match(pyversion, "command not found") then
+	load("python")
+else
+	_, _, v1, v2, _ = strfind( pyversion, "(%d+)%.(%d+)%.(%d+)" )
+	if v1 < 2 or (v1 == 2 and v2 < 7) then
+		load("python")
+	end
+end
 
 -- Load module dependencies
 local software_path = '/work/01329/poldrack/software'
 
 -- Create environment variables.
-local bar_dir           = pathJoin(software_path, "launcher-3.0.1");
+local bar_dir           = pathJoin(software_path, "launcher-3.1.0");
 local plugin_dir        = pathJoin(bar_dir, "plugins");
 
 setenv( "LAUNCHER_DIR",                bar_dir)
