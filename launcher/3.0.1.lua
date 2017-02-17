7a6c7933e6f08c2ba16fe28573ef1f5041b2dc03
oesteban <code@oscaresteban.es>
2017-02-17 15:05:16 -0800

    add old version of launcher

--- launcher/3.0.1.lua
+++ launcher/3.0.1.lua
@@ -12,7 +12,7 @@ https://www.tacc.utexas.edu/research-development/tacc-software/the-launcher.
 ]])
 
 whatis("Name: Launcher")
-whatis("Version: 3.1.0")
+whatis("Version: 3.0.1")
 
 -- Check Python version: launcher requires >=2.7
 local pyversion = capture("python --version")
@@ -29,10 +29,10 @@ end
 local software_path = '/work/01329/poldrack/software'
 
 -- Create environment variables.
-local bar_dir           = pathJoin(software_path, "launcher-3.1.0");
+local bar_dir           = pathJoin(software_path, "launcher-3.0.1");
 local plugin_dir        = pathJoin(bar_dir, "plugins");
 
-setenv( "LAUNCHER_VERSION",            "3.1.0")
+setenv( "LAUNCHER_VERSION",            "3.0.1")
 setenv( "LAUNCHER_DIR",                bar_dir)
 setenv( "LAUNCHER_PLUGIN_DIR",         plugin_dir)
 setenv( "LAUNCHER_RMI",                "SLURM")
